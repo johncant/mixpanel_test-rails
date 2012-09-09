33ec5f5049ca3babf10dd64282f3eaebd5ebd9e1
John Cant <a.johncant@gmail.com>
2012-09-09 19:47:47 +0100

    Updated. Mainly, steps wait for events to be received.

--- lib/generators/mixpanel_test/cucumber/templates/mixpanel_steps.rb
+++ lib/generators/mixpanel_test/cucumber/templates/mixpanel_steps.rb
@@ -1,34 +1,29 @@
 #When
 
 # Ignore a mixpanel event
-When /^I discard that Mixpanel event$/ do
-  ev = nil
-  @mixpanel_test_service.transaction do
-    ev = @mixpanel_test_service.events.shift
-  end
-
-  assert_not_nil(ev, "Mixpanel did not receive any events")
-
-end
-
-# Check that Mixpanel received any event
-Then /^(?:|Mixpanel )should receive an event$/ do
-  @mixpanel_test_service.transaction do
-    @last_mixpanel_event = @mixpanel_test_service.events.shift
-    assert_not_nil(@last_mixpanel_event, "Mixpanel did not receive any events")
-  end
+When /^I discard that Mixpanel "([^\"]*)" event$/ do |event_name|
+  step "Mixpanel should receive a \"#{event_name}\" event"
 end
 
 # Check that Mixpanel received a specific event
 Then /^(?:|Mixpanel )should receive (?:|a|an) "([^\"]*)" event$/ do |event_name|
-  @mixpanel_test_service.transaction do
-    matching_events = @mixpanel_test_service.events.find_all do |ev|
-      ev["event"] == event_name
+  @last_mixpanel_event = mixpanel_synchronize_until do
+
+    mixpanel_test_service.transaction do
+      matching_events = mixpanel_test_service.events.find_all do |ev|
+        ev["event"] == event_name
+      end
+      assert matching_events.count <= 1, "More than one \"#{event_name}\" events were received"
+
+      if matching_events.count == 1
+        next mixpanel_test_service.events.delete(matching_events.first)
+      else
+        next false
+      end
     end
-    assert matching_events.count <= 1, "More than one \"#{event_name}\" events were received"
-    assert_not_nil matching_events.first, "Mixpanel did not receive any events"
-    @last_mixpanel_event = @mixpanel_test_service.events.delete(matching_events.first)
   end
+
+  assert_not_nil @last_mixpanel_event, "Mixpanel did not receive any \"#{event_name}\" events"
 end
 
 # Check that a page visit was recorded
@@ -42,11 +37,21 @@ end
 
 # Check that Mixpanel will show something up in the stream
 Then /^(?:|Mixpanel )should show "([^\"]*)" in the stream(?: and (?:|as |for )the "([^\"]*)" property)?$/ do |value, key|
-  step "Mixpanel should see \"#{value}\" as the \"mp_notes\" property"
+  step "Mixpanel should see \"#{value}\" as the \"mp_note\" property"
   step "Mixpanel should see \"#{value}\" as the \"#{key}\" property" if key
 end
 
 # Check that a url is used as a Mixpanel property
 Then /^(?:|Mixpanel )should see the url for (.+?) (?:|as |for )the "([^\"]*)" property$/ do |path, key|
   step "Mixpanel should see \"#{path_to(path)}\" as the \"#{key}\" property"
+end
+
+Then /^the Mixpanel stream should contain "([^\"]*)"$/ do |text|
+  notes = @last_mixpanel_event["properties"]["mp_note"]
+  assert notes.include?(text), "Expected property mp_note (\"#{notes}\") to contain \"#{text}\""
+end
+
+Then /^the Mixpanel stream should contain the word "([^\"]*)"$/ do |test|
+  notes = @last_mixpanel_event["properties"]["mp_note"]
+  assert notes.match(Regexp.new(/(\W|^)#{Regexp.escape(test)}(\W|$)/)), 
 end
--- lib/generators/mixpanel_test/cucumber/templates/mixpanel_test_service.rb
+++ lib/generators/mixpanel_test/cucumber/templates/mixpanel_test_service.rb
@@ -1,18 +1,40 @@
 # Mixpanel test service.
 # Use this with my Mixpanel gem fork (johncant/mixpanel) or replace the mixpanel javascript file url hostname with localhost:3001. This will proxy the Mixpanel javascript file through the Mixpanel test service, which will edit it slightly. Then, all Mixpanel API calls will go to the Mixpanel test service instead, where you can run tests on them.
 require "mixpanel_test/service"
+require "test/unit"
 
-mixpanel_test_service = nil
+module MixpanelTestHelpers
+  mattr_accessor :mixpanel_test_service
+
+  def mixpanel_synchronize_until(secs = 2)
+    start_time = Time.now
+
+    until (res=yield) || Time.now >= start_time+2.seconds
+      sleep 0.05
+    end
+
+    res || nil
+  end
+
+end
+
+World(MixpanelTestHelpers)
 
 Before "@mixpanel" do
-  mixpanel_test_service ||= MixpanelTest::Service.new(:port => 3001) # Don't shut this down until the end of the process. There are likely to be so many mixpanel tests that the overhead would really slow down the tests.
-  @mixpanel_test_service = mixpanel_test_service
-
-  @mixpanel_test_service.transaction do
-    @mixpanel_test_service.events.clear
+  begin
+    MixpanelTestHelpers.mixpanel_test_service ||= MixpanelTest::Service.new(:port => 3001) # Don't shut this down until the end of the process. There are likely to be so many mixpanel tests that the overhead would really slow down the tests.
+    MixpanelTestHelpers.mixpanel_test_service.transaction do
+      MixpanelTestHelpers.mixpanel_test_service.events.clear
+    end
+  rescue
+    MixpanelTestHelpers.mixpanel_test_service = nil
   end
 end
+
 at_exit do
-  mixpanel_test_service.stop
-  Thread.pass until mixpanel_test_service.stopped?
+  if MixpanelTestHelpers.mixpanel_test_service
+    MixpanelTestHelpers.mixpanel_test_service.stop
+    Thread.pass until MixpanelTestHelpers.mixpanel_test_service.stopped?
+  end
 end
+
