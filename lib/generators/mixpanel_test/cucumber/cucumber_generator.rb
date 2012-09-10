4ad7815ee9ae12edd59576a0cf4f7aaea47670c1
John Cant <a.johncant@gmail.com>
2012-09-10 09:08:08 +0100

    Fixed require problem

--- lib/generators/mixpanel_test/cucumber/cucumber_generator.rb
+++ lib/generators/mixpanel_test/cucumber/cucumber_generator.rb
@@ -1,12 +1,14 @@
-class MixpanelTest::CucumberGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+module MixpanelTest
+  class CucumberGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-  def generate_cucumber
-    copy_file "mixpanel_steps.rb", "features/step_definitions/mixpanel_steps.rb"
-    copy_file "mixpanel_test_service.rb", "features/support/mixpanel_test_service.rb"
-    puts "(1a) If you're using the mixpanel gem, then set the config parameter :url to \"localhost:3001\" and the :test_service_hostname to \"localhost:3001\" (the johncant/mixpanel fork definitely contains these options)"
-    puts "(1b) If you're not using the mixpanel gem, then replace the hostname in the Mixpanel javascript file url with \"localhost:3001\"."
-    puts "PROFIT!!!!!! mixpanel_test_service will then hijack all the API data for the purpose of testing."
-    puts "If that didn't work, please change this gem or mixpanel_test_service or mixpanel and submit a pull request."
+    def generate_cucumber
+      copy_file "mixpanel_steps.rb", "features/step_definitions/mixpanel_steps.rb"
+      copy_file "mixpanel_test_service.rb", "features/support/mixpanel_test_service.rb"
+      puts "(1a) If you're using the mixpanel gem, then set the config parameter :url to \"localhost:3001\" and the :test_service_hostname to \"localhost:3001\" (the johncant/mixpanel fork definitely contains these options)"
+      puts "(1b) If you're not using the mixpanel gem, then replace the hostname in the Mixpanel javascript file url with \"localhost:3001\"."
+      puts "PROFIT!!!!!! mixpanel_test_service will then hijack all the API data for the purpose of testing."
+      puts "If that didn't work, please change this gem or mixpanel_test_service or mixpanel and submit a pull request."
+    end
   end
 end
